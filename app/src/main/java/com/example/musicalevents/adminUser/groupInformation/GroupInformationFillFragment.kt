7c6d302ef019c896173cc2b096a8a92607a7c227
Rafael Rio <rafa2000inator@gmail.com>
2022-11-14 19:27:01 +0100

    Refactorizado utilskt y funciona la informacion del grupo

--- app/src/main/java/com/example/musicalevents/adminUser/groupInformation/GroupInformationFillFragment.kt
+++ app/src/main/java/com/example/musicalevents/adminUser/groupInformation/GroupInformationFillFragment.kt
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.navArgs
 import com.example.musicalevents.R
 import com.example.musicalevents.data.model.Userkt
 import com.example.musicalevents.databinding.FragmentGroupFillInformationBinding
+import com.example.musicalevents.utils.UtilsKt
 import com.google.firebase.firestore.FirebaseFirestore
 
 class GroupInformationFillFragment : Fragment() {
@@ -31,7 +32,6 @@ class GroupInformationFillFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGroupFillInformationBinding.inflate(inflater, container, false)
-        bindingFields()
         return binding.root
     }
 
@@ -40,20 +40,9 @@ class GroupInformationFillFragment : Fragment() {
         bindingFields()
         binding.apply {
             btAcceptLinks.setOnClickListener {
-                editedUser.email?.let { it1 ->
-                    db.collection("personas").document(it1).set(
-                        hashMapOf(
-                            "email" to editedUser.email,
-                            "admin" to editedUser.isAdmin,
-                            "password" to editedUser.password,
-
-                            "instagram" to etInsta.text.toString(),
-                            "twitter" to etTwitter.text.toString(),
-                            "facebook" to etFacebook.text.toString(),
-                            "website" to etWebsite.text.toString(),))
-                }
+                editUser()
                 Toast.makeText(context, R.string.updatedLinks, Toast.LENGTH_SHORT).show()
-                //findNavController().navigateUp()
+                findNavController().navigateUp()
             }
 
         }
@@ -65,6 +54,31 @@ class GroupInformationFillFragment : Fragment() {
             etTwitter.setText(editedUser.twitter)
             etFacebook.setText(editedUser.facebook)
             etWebsite.setText(editedUser.website)
+        }
+    }
+
+    private fun editUser() {
+        binding.apply {
+            editedUser.email?.let { it1 ->
+                db.collection(UtilsKt.personasTable).document(it1).set(
+                    hashMapOf(
+                        "email" to editedUser.email,
+                        "admin" to editedUser.isAdmin,
+                        "password" to editedUser.password,
+
+                        "instagram" to etInsta.text.toString(),
+                        "twitter" to etTwitter.text.toString(),
+                        "facebook" to etFacebook.text.toString(),
+                        "website" to etWebsite.text.toString(),
+                    )
+                )
+            }
+        }
+        binding.apply {
+            editedUser.instagram = etInsta.text.toString()
+            editedUser.twitter = etTwitter.text.toString()
+            editedUser.facebook = etFacebook.text.toString()
+            editedUser.website = etWebsite.text.toString()
         }
     }
 }
--- app/src/main/java/com/example/musicalevents/utils/UtilsKt.kt
+++ app/src/main/java/com/example/musicalevents/utils/UtilsKt.kt
@@ -8,45 +8,38 @@ import java.util.regex.Pattern
 class UtilsKt {
 
 
-
-    companion object{
+    companion object {
+        //Constantes de firebase
         val eventosTable = "eventos"
         val personasTable = "personas"
+
+
         fun setDateHour(text: TextInputEditText, hour: TextInputEditText, calendar: Calendar) {
-            text.setText(
-                String.format("%02d", calendar[Calendar.DAY_OF_MONTH]) + "/" + String.format(
-                    "%02d",
-                    calendar[Calendar.MONTH] + 1
-                ) + "/" + String.format(
-                    "%04d",
-                    calendar[Calendar.YEAR]
-                )
-            )
-            hour.setText(
-                (String.format("%02d", calendar[Calendar.HOUR_OF_DAY]) + ":" + String.format(
-                    "%02d",
-                    calendar[Calendar.MINUTE]
-                ))
-            )
+            val dayOfMonth = String.format("%02d", calendar[Calendar.DAY_OF_MONTH])
+            val month = String.format("%02d", calendar[Calendar.MONTH] + 1)
+            val year = String.format("%04d", calendar[Calendar.YEAR])
+            val hourOfDay = String.format("%02d", calendar[Calendar.HOUR_OF_DAY])
+            val minute = String.format("%02d", calendar[Calendar.MINUTE])
+
+            text.setText("$dayOfMonth/$month/$year")
+            hour.setText("$hourOfDay:$minute")
         }
 
         fun setDateHour(text: TextView, hour: TextView, calendar: Calendar) {
-            text.text = String.format("%02d", calendar[Calendar.DAY_OF_MONTH]) + "/" + String.format(
-                "%02d",
-                calendar[Calendar.MONTH] + 1
-            ) + "/" + String.format(
-                "%04d",
-                calendar[Calendar.YEAR]
-            )
-            hour.text = (String.format("%02d", calendar[Calendar.HOUR_OF_DAY]) + ":" + String.format(
-                "%02d",
-                calendar[Calendar.MINUTE]
-            ))
+            val dayOfMonth = String.format("%02d", calendar[Calendar.DAY_OF_MONTH])
+            val month = String.format("%02d", calendar[Calendar.MONTH] + 1)
+            val year = String.format("%04d", calendar[Calendar.YEAR])
+            val hourOfDay = String.format("%02d", calendar[Calendar.HOUR_OF_DAY])
+            val minute = String.format("%02d", calendar[Calendar.MINUTE])
+
+            text.text = "$dayOfMonth/$month/$year"
+            hour.text = "$hourOfDay:$minute"
         }
 
         @JvmStatic
-        fun isPasswordValid(password: String) : Boolean{
-            val PASSWORDPATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$"
+        fun isPasswordValid(password: String): Boolean {
+            val PASSWORDPATTERN =
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$"
             val pattern = Pattern.compile(PASSWORDPATTERN);
             val matcher = pattern.matcher(password);
             return matcher.matches();
